7f828861e9cc0b3543b249364fd04e3aa2f2273c
Xav86 <gustavo.g.2005@gmail.com>
2023-07-10 21:17:54 -0300

    feitos exercicio06 e 07

--- exercicio06/index.php
+++ exercicio06/index.php
@@ -6,15 +6,78 @@
     <title>exercicio06</title>
 </head>
 <body>
+    <?php
+         $altura = $_POST["altura"]??null;
+         $peso = $_POST["peso"]??null;
+
+         $altura = (double)$altura;
+         $peso = (double)$peso;
+
+         if (isset($_POST["clear"]))
+         {
+            $altura = "";
+            $peso = "";
+         }
+
+    ?>
     
     <form action="<?=$_SERVER["PHP_SELF"]?>" method="post">
+        
+    <h1>Programa pra calcular IMC, peso/altura², e segue a tabela do q tem q mostra . - abaixo de 18.5: Abaixo do peso - entre 18.5 e 25: Peso ideal - entre 25 e 30: Sobrepeso - entre 30 e 40: Obesidade - acima de 40: Obesidade mórbida</h1>
+    <table>
+        <tr>
+            <td>Digite sua altura</td>
+            <td><input type="text" name="altura"></td>
 
+        </tr>
 
+        <tr>
+            <td>Digite seu peso</td>
+            <td><input type="text" name="peso"></td>
 
+        </tr>
 
+    </table>
 
+    <button type="submit" name="btn">Executar</button>
+    <button type="submit" name="clear">Limpar</button>
 
     </form>
+
+<?php
+if (isset($_POST["btn"])) //falta corrigir o erro de não aceitar os numeros com virgua :( o qual causa outros problemas aparentemente...
+{
+
+    if (($altura == "") || ($peso == ""))
+    {
+        echo "Preencha todos os campos";
+
+    } else {
+        $imc = $peso/($altura*$altura);
+        if ($imc <= 18.5)
+        {
+            echo "Você esta abaixo do peso ideal, seu imc é $imc";
+        } else if (($imc > 18.5) &&($imc <= 25))
+        {
+            echo "Você esta no peso ideal, seu imc é $imc";
+        } else if (($imc > 25) && ($imc <= 30))
+        {
+            echo "Você esta com sobre-peso, seu imc é $imc";
+        } else if (($imc > 30) && ($imc <= 40))
+        {
+            echo "Você esta obeso, seu imc é $imc";
+        } else if ($imc > 40)
+        {
+            echo "Você esta com obesidade morbida, seu imc é $imc";
+        } else 
+        {
+            echo "Como chegou aqui?";
+        }
+
+    }
+}
+
+?>
     
 </body>
 </html>
--- exercicio07/index.php
+++ exercicio07/index.php
@@ -6,15 +6,95 @@
     <title>exercicio07</title>
 </head>
 <body>
+<?php
+    $carro = $_POST["carro"]??null;
+    $dias = $_POST["dias"]??null;
+    $km = $_POST["km"]??null;
+    if (isset($_POST["clear"]))
+    {
+        $carro = "";
+        $dias = "";
+        $km = "";
+
+    }
+
+?>
     
     <form action="<?=$_SERVER["PHP_SELF"]?>" method="post">
+    <h1>prgrama le tipo de carro, quanto dias foi alugado e o tipo, luxo 150por dia ou popular 90por dia e os quilometro. ent segue tabela Carros populares (aluguel de R$90 por dia) - Até 100Km percorridos: R$0,20 por Km - Acima de 100Km percorridos: R$0,10 por Km - Carros de luxo (aluguel de R$150 por dia) - Até 200 Km percorridos: R$0,30 por Km - Acima de 200Km percorridos: R$0,25 por Km.</h1>
+    <table>
+        <tr>
+            <td>Digite tipo de carro (Popular/Luxo)</td>
+            <td><input type="text" name="carro"></td>
 
+        </tr>
 
+        <tr>
+            <td>Digite dias de uso</td>
+            <td><input type="text" name="dias"></td>
 
+        </tr>
 
+        <tr>
+            <td>Digite os quilometros percorridos</td>
+            <td><input type="text" name="km"></td>
 
+        </tr>
+
+    </table>
+
+    <button type="submit" name="btn">Executar</button>
+    <button type="submit" name="clear">Limpar</button>
 
     </form>
+<?php
+if (isset($_POST["btn"]))
+{
+    if (($carro == "") || ($dias == "") || ($km == ""))
+    {
+        echo "preencha todos os campos";
+    } else {
+        if (($carro == "Popular") || ($carro == "popular"))
+        {
+            $aluguel = 90*$dias;
+                //aqui n vai funfa pq n manjo d double... AINDA...
+            if ($km <= 100) 
+            {
+                $total = $aluguel+0.20;
+                echo "Total do aluguel do carro ficou $total";
+            } else if ($km > 100)
+            {
+                $extra = $km/100;
+                $extra = $extra*0.10;
+                $total = $aluguel+0.20+$extra;
+                echo "Total do aluguel do carro ficou $total";
+                
+            }
+
+        } else if (($carro == "Luxo") || ($carro == "luxo")) {
+            $aluguel = 150*$dias;
+                //aqui n vai funfa pq n manjo d double... AINDA...
+            if ($km <= 200) 
+            {
+                $total = $aluguel+0.30;
+                echo "Total do aluguel do carro ficou $total";
+            } else if ($km > 100)
+            {
+                $extra = $km/100;
+                $extra = $extra*0.25;
+                $total = $aluguel+0.30+$extra;
+                echo "Total do aluguel do carro ficou $total";
+                
+            }
+
+        } else{
+            echo "favor digita direito, já q esse programa n é bem feito e da problema se digitarem errado";
+        }
+    
+    }    
+}
+
+?>
     
 </body>
 </html>
